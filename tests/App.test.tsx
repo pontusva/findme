bda9844bda235b777a71f2e4275aaad8438fa9cb
Pontus Abrahamsson <ponabr123@gmail.com>
2025-01-11 13:26:48 +0100

    Remove tests folder

--- tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import App from '../src/App'
-import { describe, it } from 'vitest'
-import { render } from './test-utils'
-describe('App', () => {
-  it('Renders the App component', () => {
-    render(<App />)
-  })
-})
