76e67d68ad572423724d0877b9bd5a798dec1875
Pontus Abrahamsson <ponabr123@gmail.com>
2025-01-13 07:59:43 +0100

    init: Chat window

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useUser } from "@clerk/clerk-react";
 import { useNotifications } from "@/data/useNotifications";
 import { usePushNotifications } from "../hooks/usePushNotifications";
-
+import { Card, CardContent, CardDescription } from "./ui/card";
+import { Button } from "./ui/button";
+import ChatWindow from "./chat-window";
 const Dashboard: React.FC = () => {
   const { user } = useUser();
   const { notifications } = useNotifications();
@@ -25,16 +27,20 @@ const Dashboard: React.FC = () => {
           <h2 className="font-semibold">Recent Searches</h2>
           <p>You haven't performed any searches yet.</p>
         </div>
-        <div className="bg-yellow-100 p-4 rounded-md">
+        <div className="bg-yellow-100 p-4 max-h-80 overflow-y-auto rounded-md">
           <h2 className="font-semibold">Subscriptions</h2>
           {notifications.length > 0 ? (
             notifications.map((subscription, index) => (
-              <div key={subscription.id + index}>
-                <p>{subscription.id}</p>
-                <p>{subscription.message}</p>
-                <p>{subscription.sender?.name}</p>
-                <p>{subscription.sender?.email}</p>
-              </div>
+              <Card className="mt-5" key={subscription.id + index}>
+                <CardContent className="mt-5">
+                  <CardDescription>
+                    <p>{subscription.sender?.email}</p>
+                  </CardDescription>
+                  <p>{subscription.message}</p>
+                  <p>{subscription.sender?.name}</p>
+                  <Button className="mt-5">Start chat</Button>
+                </CardContent>
+              </Card>
             ))
           ) : (
             <p>No active subscriptions at this time.</p>
@@ -56,6 +62,7 @@ const Dashboard: React.FC = () => {
       {subscription && (
         <p className="text-green-500">Push notifications enabled!</p>
       )}
+      <ChatWindow />
     </div>
   );
 };
